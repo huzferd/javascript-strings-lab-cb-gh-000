5a65cfde5d58b2c4c84f275514bf2c9cffdb1488
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-17 10:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var greeting = `Hello, ${everybody}!`;
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = `${greeting} ${specialGuest}!`;
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = "space";
 

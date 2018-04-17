817fbe22112555a49d5355546f082244c872c84e
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-17 10:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var everybody = "everybody";
 
-var greeting = `Hello, ${everybody!`;
+var greeting = `Hello, ${everybody}!`;
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = `${greeting} ${specialGuest}!`;
 
